621e4d02d712bbba05662841f2212a505554d49d
Mehedi Nayeem <87238832+mehedihnayeem@users.noreply.github.com>
2023-09-02 10:58:46 +0600

    image loading fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import bdLogo from "../images/bd_logo.png";
 export default function NavBar() {
   return (
     <div className="nab-bar">
       <div>
-        <img src="src/images/bd_logo.png" alt="" />
+        <img src={bdLogo} alt="logo" />
       </div>
       <div>
         <h1>WEB BASED GPA CALCULATION SYSTEM FOR EDUCATION BOARDS</h1>
